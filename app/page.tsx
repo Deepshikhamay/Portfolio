561c8de02b999cd4460c0b65dadfd1b16b0c9ba1
Deepshikha Singh <115532468+Deepshikhamay@users.noreply.github.com>
2024-03-26 02:30:01 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="bg-slate-800">
+    <div className="bg-[#000]">
       <div className="">
         <Header />
       </div>
